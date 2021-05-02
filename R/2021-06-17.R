a16a215dcf7a144a81f47371cd7db98e590040c5
Mickaël Canouil <mickael.canouil@cnrs.fr>
2021-05-02 13:46:08 +0200

    add company url

--- R/2021-06-17.R
+++ R/2021-06-17.R
@@ -4,7 +4,7 @@ social(
   rmd_params = list(
     title = 'Reproducible Computation at Scale in R with {targets}',
     author = "Will M. Landau",
-    institute = 'Eli Lily and Company',
+    institute = 'Eli Lily and Company ([www.lilly.com](https://www.lilly.com/))',
     date = 'Thursday, the 17<sup><i style="font-size:0.5em;">th</i></sup> of June, 2021 - 18:00 CEST / 12:00 EDT',
     picture = "https://avatars.githubusercontent.com/u/1580860",
     website = "[wlandau.github.io](https://wlandau.github.io/)"
